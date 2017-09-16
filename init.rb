c4262ac8adf90e013d54e09baf7af79014a1e210
aulandsdalen <aulandsdalen@gmail.com>
2017-09-16 23:29:57 +0300

    add init tables script

---
+++ init.rb
@@ -0,0 +1,24 @@
+require 'sequel'
+
+print "postgres user? "
+dbuser = gets.chomp
+print "postgres password? "
+dbpassword = gets.chomp 
+print "postgres database? "
+dbname = gets.chomp
+
+DB = Sequel.connect("postgres://#{dbuser}:#{dbpassword}@localhost:5432/#{dbname}")
+
+DB.create_table :synology do
+	primary_key :id
+	String :name
+	Bigint :size
+	String :md5
+end
+
+DB.create_table :recoveredfiles do
+	primary_key :id
+	String :name
+	Bigint :size
+	String :md5
+end
