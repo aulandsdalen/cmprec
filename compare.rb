14d8a82cda99f9d025b657e1fb63c93007af332d
aulandsdalen <aulandsdalen@gmail.com>
2017-09-17 18:09:44 +0300

    search for remote files

--- compare.rb
+++ compare.rb
@@ -17,4 +17,16 @@ recoveredfiles = DB[:recoveredfiles]
 remotefiles_arr = remotefiles.all
 recoveredfiles_arr = recoveredfiles.all
 
-rd = RDispatch.new
+# rd = RDispatch.new
+recoveredfiles_arr.each do |file|
+	remote_sibling = remotefiles.where(:name => file[:name])
+	unless remote_sibling.nil?
+		if (remote_sibling[:md5] == file[:md5])
+			puts "#{remote_sibling[:name]} = #{file[:name]}"
+		else
+			puts "different hash for #{remote_sibling[:name]}"
+		end
+	else
+		puts "#{file[:name]} was not found on server"
+	end
+end
