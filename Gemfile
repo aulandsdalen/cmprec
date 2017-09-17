ef7ccf399ee1ef1792aa23851bc19e913ed3500d
aulandsdalen <aulandsdalen@gmail.com>
2017-09-17 17:59:35 +0300

    modify compare script

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org' do
 	gem 'pg'
 	gem 'sequel'
 	gem 'progress_bar'
+	gem 'rdispatch'
 end
--- compare.rb
+++ compare.rb
@@ -1,3 +1,20 @@
 require 'csv'
 require 'sequel'
+require 'rdispatch'
 
+print "postgres user? "
+dbuser = gets.chomp
+print "postgres password? "
+dbpassword = gets.chomp 
+print "postgres database? "
+dbname = gets.chomp
+
+DB = Sequel.connect("postgres://#{dbuser}:#{dbpassword}@localhost:5432/#{dbname}")
+
+remotefiles = DB[:remotefiles]
+recoveredfiles = DB[:recoveredfiles]
+
+remotefiles_arr = remotefiles.all
+recoveredfiles_arr = recoveredfiles.all
+
+rd = RDispatch.new
