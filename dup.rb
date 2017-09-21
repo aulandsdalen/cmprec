54ee517571e86f8cea8fc3770c309d08bb5362ef
aulandsdalen <aulandsdalen@gmail.com>
2017-09-21 16:35:59 +0300

    fix file write mode

--- dup.rb
+++ dup.rb
@@ -23,7 +23,7 @@ remotefiles = DB[:remotefiles]
 puts "Loading list of files..."
 files_array = remotefiles.all
 
-dupbar = ProgressBar.new(files_array.length, :bar, :counter, :percentage, :elapsed,)
+dupbar = ProgressBar.new(files_array.length)
 files_processed = 0
 
 files_array.each do |file|
@@ -34,7 +34,7 @@ files_array.each do |file|
 	if similar_files.count == 1
 		# no files found
 		IO.write(LOGFILE, "#{files_processed}: no duplicates for #{file[:name]}\n", mode: 'a')
-		IO.write(NODUPCSV, "#{file[:name]};#{file[:size]};#{file[:md5]}\n")
+		IO.write(NODUPCSV, "#{file[:name]};#{file[:size]};#{file[:md5]}\n", mode: 'a')
 	else
 		similar_files.each do |sf|
 			if sf[:md5] == file[:md5]
@@ -43,16 +43,16 @@ files_array.each do |file|
 					IO.write(LOGFILE, "#{files_processed}: no duplicates for file #{file[:name]}\n", mode: 'a')
 				else
 					# #4
-					IO.write(LOGFILE, "#{files_processed}: duplicate found for #{file[:name]} at #{sf[:name]}\n")
-					IO.write(DUPCSV, "#{file[:name]};#{file[:size]};#{file[:md5]};#{sf[:name]};#{sf[:size]};#{sf[:md5]}\n")
+					IO.write(LOGFILE, "#{files_processed}: duplicate found for #{file[:name]} at #{sf[:name]}\n", mode: 'a')
+					IO.write(DUPCSV, "#{file[:name]};#{file[:size]};#{file[:md5]};#{sf[:name]};#{sf[:size]};#{sf[:md5]}\n", mode: 'a')
 				end
 			elsif sf[:name] == file[:name]
 				# WTF??
-				IO.write(LOGFILE, "#{files_processed}: WTF?? #{file[:name]}\n")
+				IO.write(LOGFILE, "#{files_processed}: WTF?? #{file[:name]}\n", mode: 'a')
 			else
 				# #2
-				IO.write(LOGFILE, "#{files_processed}: duplicate with different md5 found for #{file[:md5]}\n")
-				IO.write(DIFFCSV, "#{file[:name]};#{file[:size]};#{file[:md5]};#{sf[:name]};#{sf[:size]};#{sf[:md5]}\n")
+				IO.write(LOGFILE, "#{files_processed}: duplicate with different md5 found for #{file[:md5]}\n", mode: 'a')
+				IO.write(DIFFCSV, "#{file[:name]};#{file[:size]};#{file[:md5]};#{sf[:name]};#{sf[:size]};#{sf[:md5]}\n", mode: 'a')
 			end	
 		end
 	end
