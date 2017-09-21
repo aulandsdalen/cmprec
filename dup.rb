fff2ed6436133b16a2d2ae8593e2bdaf741eaa91
aulandsdalen <aulandsdalen@gmail.com>
2017-09-21 16:52:56 +0300

    add branch id to log

--- dup.rb
+++ dup.rb
@@ -33,26 +33,26 @@ files_array.each do |file|
 	files_processed += 1
 	if similar_files.count == 1
 		# no files found
-		IO.write(LOGFILE, "#{files_processed}: no duplicates for #{file[:name]}\n", mode: 'a')
+		IO.write(LOGFILE, "NOFILESFOUND #{files_processed}: no duplicates for #{file[:name]}\n", mode: 'a')
 		IO.write(NODUPCSV, "#{file[:name]};#{file[:size]};#{file[:md5]}\n", mode: 'a')
 	else
 		similar_files.each do |sf|
 			if sf[:md5] == file[:md5]
 				if sf[:name] == file[:name]
 					# #3
-					IO.write(LOGFILE, "#{files_processed}: no duplicates for file #{file[:name]}\n", mode: 'a')
+					IO.write(LOGFILE, "BRANCH3 #{files_processed}: no duplicates for file #{file[:name]}\n", mode: 'a')
 					IO.write(NODUPCSV, "#{file[:name]};#{file[:size]};#{file[:md5]}\n", mode: 'a')
 				else
 					# #4
-					IO.write(LOGFILE, "#{files_processed}: duplicate found for #{file[:name]} at #{sf[:name]}\n", mode: 'a')
+					IO.write(LOGFILE, "BRANCH4 #{files_processed}: duplicate found for #{file[:name]} at #{sf[:name]}\n", mode: 'a')
 					IO.write(DUPCSV, "#{file[:name]};#{file[:size]};#{file[:md5]};#{sf[:name]};#{sf[:size]};#{sf[:md5]}\n", mode: 'a')
 				end
 			elsif sf[:name] == file[:name]
 				# WTF??
-				IO.write(LOGFILE, "#{files_processed}: WTF?? #{file[:name]}\n", mode: 'a')
+				IO.write(LOGFILE, "BRANCHWTF #{files_processed}: WTF?? #{file[:name]}\n", mode: 'a')
 			else
 				# #2
-				IO.write(LOGFILE, "#{files_processed}: duplicate with different md5 found for #{file[:name]}\n", mode: 'a')
+				IO.write(LOGFILE, "BRANCH2 #{files_processed}: duplicate with different md5 found for #{file[:name]}\n", mode: 'a')
 				IO.write(DIFFCSV, "#{file[:name]};#{file[:size]};#{file[:md5]};#{sf[:name]};#{sf[:size]};#{sf[:md5]}\n", mode: 'a')
 			end	
 		end
